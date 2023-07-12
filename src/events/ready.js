ff16da87e630e3cf665fba66021a0d1880989ce1
hungsean <hungsean0419@gmail.com>
2023-07-12 15:42:23 +0800

    要大寫

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity('有沒有bug', { type: ActivityType.watching });
+		client.user.setActivity('有沒有bug', { type: ActivityType.Watching });
 		client.user.setAvatar('./avatar.png');
 
 	},
