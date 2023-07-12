8cb7a87def70d55f672b22a64c8a94ea613fb5cf
hungsean <hungsean0419@gmail.com>
2023-07-12 16:25:56 +0800

    嘗試看看messages.cache

--- src/messageCreateCommands/replyHi/index.js
+++ src/messageCreateCommands/replyHi/index.js
@@ -7,5 +7,8 @@ module.exports = {
         if (message.content !== 'hi') return;
         console.log(`[info] messageCreate: replyHi`)
         await message.reply('hi');
+        const messages = message.channel.messages.cache;
+        console.log(`[info] messages:`);
+        console.log(messages);
     },
 };
