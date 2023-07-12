a06da13370e6481184b02d8b152de6ddbc816069
hungsean <hungsean0419@gmail.com>
2023-07-12 15:42:39 +0800

    就是個debug用的東西

--- src/messageCreateCommands/twitterEmbeds/index.js
+++ src/messageCreateCommands/twitterEmbeds/index.js
@@ -5,17 +5,11 @@ module.exports = {
         name: 'twitterEmbeds',
     },
     async execute(message) {
-        if (!message.content.startsWith(process.env.VXTWITTER_URL_PREFIX)) return;
-        if (message.embeds === null) return;
+        if (!message.content.startsWith(process.env.TWITTER_URL_PREFIX)) return;
+        if (message.embeds.length === 0) return;
         console.log(`[info] messageCreate: twitterEmbeds`);
         console.log(`[info] embeds[0]: `);
         console.log(message.embeds[0]);
-        console.log(`[info] embeds[0].author: `);
-        console.log(message.embeds[0].author);
-        console.log(`[info] embeds[0].fields: `);
-        console.log(message.embeds[0].fields);
-        console.log(`[info] embeds[0].footer: `);
-        console.log(message.embeds[0].footer);
         console.log(`[info] embeds[0].image: `);
         console.log(message.embeds[0].image);
 
