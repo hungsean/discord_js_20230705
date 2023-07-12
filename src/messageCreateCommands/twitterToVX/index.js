afe49a38704f5c779032237c1097d76712c96afb
hungsean <hungsean0419@gmail.com>
2023-07-12 15:43:19 +0800

    想辦法增加對image為空的檢測，以及嘗試做embed

--- src/messageCreateCommands/twitterToVX/index.js
+++ src/messageCreateCommands/twitterToVX/index.js
@@ -1,3 +1,5 @@
+const { EmbedBuilder } = require("@discordjs/builders");
+
 module.exports = {
     data:
     { 
@@ -5,11 +7,25 @@ module.exports = {
     },
     async execute(message) {
         if (!message.content.startsWith(process.env.TWITTER_URL_PREFIX)) return;
-        if (message.embeds !== null) return;
+
+        if (message.embeds.length === 0) ;
+        else if (message.embeds[0].image === null) ;
+        else return;
+
         console.log(`[info] messageCreate: twitterToVX`)
         const content = message.content;
         const update = content.replace(process.env.TWITTER_URL_PREFIX, process.env.VXTWITTER_URL_PREFIX);
-        await message.channel.send(`${message.member.displayName} ${update}`);
+        const updateMessage = await message.channel.send(`${message.member.displayName} ${update}`);
+        if (updateMessage.embeds.length === 0) ;
+        {
+            await updateMessage.delete();
+            const embed = new EmbedBuilder()
+                .setTitle('Error')
+                .setDescription('The vxtwitter is not available.')
+                .setColor(0xff0000)
+                .setTimestamp();
+            await message.channel.send({ embeds: [embed] });
+        }
     },
 };
 
