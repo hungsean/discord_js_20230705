ad0671462c3e9a506526d6bb25ae46027ade22ac
hungsean <hungsean0419@gmail.com>
2023-07-23 00:32:12 +0800

    新增個end

--- src/interactionCreateCommands/rank/index.js
+++ src/interactionCreateCommands/rank/index.js
@@ -8,12 +8,21 @@ module.exports = {
             subcommand
                 .setName('start')
                 .setDescription('Start a rank')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('end')
+                .setDescription('End a rank')
         ),
     rankStarted: false,
     async execute(interaction) {
         if (interaction.options.getSubcommand() === 'start') {
             await interaction.reply('Rank started!');
             this.rankStarted = true;
+        }
+        else if (interaction.options.getSubcommand() === 'end') {
+            await interaction.reply('Rank ended!');
+            this.rankStarted = false;
         }
     }
 }
